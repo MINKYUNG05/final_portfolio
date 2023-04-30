5c9105ec66e898868d10d8432c6b553d600211c3
Minkyung Kim <118186510+MINKYUNG05@users.noreply.github.com>
2023-05-01 02:59:21 +0900

    23.05.01

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 $(document).ready(function () {
     $(".logo3").click(function () {
-       $(".bottom-logo-nav").slideToggle();
+        $(".bottom-logo-nav").slideToggle();
+    });
+});
+
+
+$(document).ready(function () {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        keyboard: {
+            enabled: true,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        }
+    });
+});
+
+
+$(document).ready(function () {
+    $(".i1").click(function () {
+        $(".i1-content").show("slide", { direction: "left" }, 1000);
     });
 });
