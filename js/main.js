eb8c967454f3502b1e994718d336b336e9356a7e
Minkyung Kim <118186510+MINKYUNG05@users.noreply.github.com>
2023-05-01 14:17:06 +0900

    23.05.01

--- js/main.js
+++ js/main.js
@@ -1,10 +1,34 @@
+
+// menu bt
 $(document).ready(function () {
     $(".logo3").click(function () {
         $(".bottom-logo-nav").slideToggle();
     });
 });
 
+// content hover
+// $(document).ready(function () {
+//     $(".pj-box").hover(function () {
+//         $(".pj-img").addClass('pj-img-hover-before');
+//         $(".arrow").addClass('arrow_hover');
+//     });
+// });
 
+
+// see the project bt
+
+// $(document).ready(function () {
+//     $(".arrow").mouseover(function () {
+//         $(this).addClass('arrow_hover');
+//     });
+//     $(".arrow").mouseleave(function () {
+//         $(this).removeClass('arrow_hover');
+//     });
+
+// });
+
+
+// swiperdemo
 $(document).ready(function () {
     var swiper = new Swiper(".mySwiper", {
         slidesPerView: 1,
@@ -24,6 +48,7 @@ $(document).ready(function () {
 });
 
 
+// index animation
 $(document).ready(function () {
     $(".i1").click(function () {
         $(".i1-content").show("slide", { direction: "left" }, 1000);
